df96e6acf06892463a22a3bd0f22abafc96da2ca
MUstinov77 <m.ustinov2001@yandex.ru>
2025-11-18 10:38:16 +0300

    log routes for employee router

--- app/api/employees/router.py
+++ app/api/employees/router.py
@@ -2,14 +2,14 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse, Response
-from sqlalchemy import select, update
+from sqlalchemy import update
 from sqlalchemy.orm import Session
 
-from app.core.utils.db_querys import get_employee_by_id, get_workspace
+from app.core.utils.db_querys import get_employee_by_id, get_workspace, get_log_by_id
 from app.db.db import session_provider
-from app.db.models import Employee, Workspace
+from app.db.models import Employee, Workspace, Log
 
-from ..schemas import EmployeeResponse
+from ..schemas import EmployeeResponse, LogCreate
 
 router = APIRouter(
     prefix="/{workspace_id}/employees",
@@ -54,6 +54,26 @@ async def get_employee_logs(
 ):
     return employee.logs
 
+
+@router.get(
+    "/{employee_id}/logs/{log_id}",
+    response_model=LogCreate,
+    responses={
+        status.HTTP_404_NOT_FOUND: {"description": "Invalid request"}
+    }
+)
+async def get_log_by_employee_id(
+        employee: Annotated[Employee, Depends(get_employee_by_id)],
+        log: Annotated[Log, Depends(get_log_by_id)],
+):
+    if not log or not employee:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Invalid request"}
+        )
+    return log
+
+
 @router.post(
     "/",
     response_model=EmployeeResponse,
@@ -87,7 +107,10 @@ async def delete_employee(
         session: Annotated[Session, Depends(session_provider)]
 ):
     if not workspace or not employee:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"message": "Invalid employee or workspace"})
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Invalid employee or workspace"}
+        )
     session.delete(employee)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
