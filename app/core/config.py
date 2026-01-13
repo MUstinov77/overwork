2735fc7de796ad46092c9ae2c219be4527eb249f
MUstinov77 <m.ustinov2001@yandex.ru>
2026-01-13 17:23:57 +0300

    add config class, rework session to async, db type sqlite -> postgres

---
+++ app/core/config.py
@@ -0,0 +1,30 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+
+class Settings(BaseSettings):
+
+    TITLE: str = "OverworkApi"
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    @property
+    def DB_URI(self): # noqa
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parents[2].joinpath(".env")
+    )
+
+
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = Settings()
--- app/core/datastore/db.py
+++ app/core/datastore/db.py
@@ -1,36 +1,72 @@
 from contextlib import asynccontextmanager
+from typing import Annotated
 
 from fastapi import FastAPI
 from fastapi.params import Depends
 from sqlalchemy import create_engine, text
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncEngine, AsyncSession
 from sqlalchemy.orm import Session
+from app.core.config import get_settings
 
 from app.models.base import Base
 
-engine = create_engine("sqlite:///overwork.db")
+
+settings = get_settings()
+
+async_engine: AsyncEngine | None = None
+async_session_maker: async_sessionmaker[AsyncSession] | None = None
 
 
 def create_session():
+    engine = create_engine("postgresql+asyncpg://postgres:postgres@localhost:5432/internal")
     with Session(engine) as session:
-        try:
-            yield session
-            session.commit()
-        except Exception:
-            session.rollback()
-            raise
+        yield session
 
 
 def session_provider(session: Session = Depends(create_session)):
     return session
 
 
-def init_db():
-    Base.metadata.create_all(engine)
-    with Session(engine) as session:
-        session.execute(text("PRAGMA foreign_keys = ON"))
+async def init_db():
+    global  async_engine, async_session_maker
 
-def destroy_db():
-    Base.metadata.drop_all(engine)
+    if async_engine:
+        print("Database already initialized")
+        return
+
+    async_engine = create_async_engine(settings.DB_URI)
+
+    async_session_maker = async_sessionmaker(
+        async_engine
+    )
+
+
+async def destroy_db():
+    global  async_engine
+
+    if async_engine:
+        await async_engine.dispose()
+        async_engine = None
+
+
+async def get_postgres_session():
+    if not async_session_maker:
+        raise RuntimeError("Database not initialized")
+
+    async with async_session_maker() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+
+
+async def postgres_session_provider(
+        session: Annotated[AsyncSession, Depends(get_postgres_session)]
+):
+    return session
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
