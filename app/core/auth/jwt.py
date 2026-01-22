6741a1327a3ef94495d7996f8dbc761b1201c38d
MUstinov77 <m.ustinov2001@yandex.ru>
2026-01-22 14:28:47 +0300

    Create jwt service

---
+++ app/core/auth/jwt.py
@@ -0,0 +1,49 @@
+import jwt
+from jwt.exceptions import InvalidTokenError, DecodeError, ExpiredSignatureError
+from datetime import datetime, timezone, timedelta
+
+from app.core.config import get_settings, Settings
+
+
+setting = get_settings()
+
+
+
+class JWTService:
+
+    def __init__(self, settings: Settings):
+        self.auth_secret_key = settings.AUTH_SECRET_KEY
+        self.algorithm = settings.JWT_ALGORITHM
+        self.token_expire = settings.TOKEN_EXPIRE
+        self.jwt_issuer = settings.JWT_ISSUER
+
+
+    def create_and_encode_token(self, user: dict):
+        now = datetime.now(timezone.utc)
+        payload = {
+            "iss": self.jwt_issuer,
+            "iat": now,
+            "exp": now + timedelta(hours=self.token_expire),
+            "context": user
+        }
+        token = jwt.encode(
+            payload,
+            self.auth_secret_key,
+            algorithm=self.algorithm
+        )
+        return token
+
+    def decode_token(self, token: str):
+        try:
+            decoded_payload = jwt.decode(
+                token,
+                self.auth_secret_key,
+                algorithms=[self.algorithm]
+            )
+            return decoded_payload
+        except ExpiredSignatureError:
+            raise ValueError("Token expired")
+        except DecodeError:
+            raise ValueError("Token decode error")
+        except InvalidTokenError:
+            raise ValueError("Invalid token")
